ada19cd2e2fb0ee2dc1d6fd77b12420e5cda9f53
saad <saad.alshamry.2012@gmail.com>
2018-10-28 23:17:36 -0400

    menu pt 2

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,222 +1,248 @@
 // menu
 
-var Burgers = [{
+var Burgers = [
+  {
     name: "Beef Burger 3oz / 6oz / 9oz",
     descreption: "Beef burger with your choice of toppings.",
     price: "4.99$ / 8.19$ / 10.69$",
-    img: "./assets/images/burger.jpg"
+    img: "./assets/images/menu/cheese-burger.jpg"
   },
   {
     name: "Cheese Burger 3oz / 6oz / 9oz",
     descreption: "Cheese burger with your choice of toppings",
     price: "5.99$ / 9.19$ / 11.69$",
-    img: "./assets/images/burger.jpg"
+    img: "./assets/images/menu/cheese-burger.jpg"
   },
   {
     name: "Double Beef Burger 6oz / 9oz",
     descreption: "Double beef burger with your choice of toppings",
     price: "11.19$ / 16.59$",
-    img: "./assets/images/burger.jpg"
+    img: "./assets/images/menu/double-cheesburger.jpg"
   },
   {
     name: "Double Cheese Burger 6oz / 9oz",
     descreption: "Double cheese burger with your choice of toppings",
     price: "12.49$ / 17.49$",
-    img: "./assets/images/burger.jpg"
+    img: "./assets/images/menu/double-cheesburger.jpg"
   },
   {
     name: "Triple Beef Burger 6oz / 9oz",
     descreption: "Triple beef burger with your choice of toppings",
     price: "14.29$ / 20.59$",
-    img: "./assets/images/burger.jpg"
+    img: "./assets/images/menu/TripleCheeseBurger.jpg"
   },
   {
     name: "Triple Cheese Burger 6oz / 9oz",
     descreption: "Triple cheese burger with your choice of toppings",
     price: "16.29$ / 22.59$",
-    img: "./assets/images/burger.jpg"
+    img: "./assets/images/menu/TripleCheeseBurger.jpg"
   },
   {
     name: "JB BBQ Burger 3oz / 6oz / 9oz",
-    descreption: "Topped with mustard, chili, sauteed mushroom and your choice of cheese, plus a crispy onion ring soaked in our JB BBQ sauce",
+    descreption:
+      "Topped with mustard, chili, sauteed mushroom and your choice of cheese, plus a crispy onion ring soaked in our JB BBQ sauce",
     price: "7.49$ / 10.39$ / 12.69$",
-    img: "./assets/images/burger.jpg"
+    img: "./assets/images/menu/JBBBQburger.jpg"
   },
   {
     name: "Pimento Cheese Burger 3oz / 6oz / 9oz",
-    descreption: "Our famouse burger topped with a big portion of gourmet home made pimento cheese and your choice of toppings",
+    descreption:
+      "Our famouse burger topped with a big portion of gourmet home made pimento cheese and your choice of toppings",
     price: "7.49$ / 10.39$ / 12.69$",
-    img: "./assets/images/burger.jpg"
+    img: "./assets/images/logo-2.png"
   },
   {
     name: "Gazebo Burger 3oz / 6oz / 9oz",
-    descreption: "kick up the flavour of your sandwich with our specially marinated burger toped with mustard, tomato, sauteed mushroom, sauteed onion and your choice of chese",
+    descreption:
+      "kick up the flavour of your sandwich with our specially marinated burger toped with mustard, tomato, sauteed mushroom, sauteed onion and your choice of chese",
     price: "7.49$ / 10.39$ / 12.69$",
-    img: "./assets/images/burger.jpg"
+    img: "./assets/images/logo-2.png"
   },
   {
     name: "PomegraNUT Burger 3oz / 6oz / 9oz",
-    descreption: "a hand made walnut beef patty, flavoured with pomegranate molasses, Swiss cheese and tomato",
+    descreption:
+      "a hand made walnut beef patty, flavoured with pomegranate molasses, Swiss cheese and tomato",
     price: "7.49$ / 10.39$ / 12.69$",
-    img: "./assets/images/burger.jpg"
+    img: "./assets/images/logo-2.png"
   },
   {
     name: "EGGcelent Burger 3oz / 6oz / 9oz",
-    descreption: "Our famous cheese burger with a fried egg on top, served with your choice of cheese, tomato, lettuce, mustard and ketchup",
+    descreption:
+      "Our famous cheese burger with a fried egg on top, served with your choice of cheese, tomato, lettuce, mustard and ketchup",
     price: "7.49$ / 10.39$ / 12.69$",
-    img: "./assets/images/burger.jpg"
+    img: "./assets/images/menu/eggburger.png"
   },
   {
     name: "Shrimp Burger 3oz / 6oz / 9oz",
-    descreption: "our famous crispy fried shrimps served with ketchup, slaw and tartar sauce on steamed burger bun",
+    descreption:
+      "our famous crispy fried shrimps served with ketchup, slaw and tartar sauce on steamed burger bun",
     price: " 5.99$ / 8.99$ / 11.69$",
-    img: "./assets/images/burger.jpg"
-  }, {
-    name: "vEGGI Burger",
-    descreption: "A spicy, flavourful black bean veggie burger comes with your choice of free toppings",
-    price: "1.2$",
-    img: "./assets/images/burger.jpg"
+    img: "./assets/images/menu/shrimp-burger.jpg"
+  },
+  {
+    name: "Veggi Burger",
+    descreption:
+      "A spicy, flavourful black bean veggie burger comes with your choice of free toppings",
+    price: "5.99$",
+    img: "./assets/images/logo-2.png"
   }
 ];
 
-var Savourysandwiches = [{
+var Savourysandwiches = [
+  {
     name: "Classic Checken Sandwich",
-    descreption: "S mouth watering chicken breast perfectly seasoned (Grilled or Fried) with mayo, lettuce and tomato in a steamed bun.",
+    descreption:
+      "S mouth watering chicken breast perfectly seasoned (Grilled or Fried) with mayo, lettuce and tomato in a steamed bun.",
     price: "7.99$",
-    img: ""
+    img: "./assets/images/logo-2.png"
   },
   {
     name: "JB Chicken Sandwich",
-    descreption: "A whole chicken breast (Grilled or Fried) layered with your choice of cheese, lettuce, tomato and mayo deliciously soaked with our signature JB BBQ sauce.",
+    descreption:
+      "A whole chicken breast (Grilled or Fried) layered with your choice of cheese, lettuce, tomato and mayo deliciously soaked with our signature JB BBQ sauce.",
     price: "8.99$",
-    img: ""
+    img: "./assets/images/logo-2.png"
   },
   {
     name: "PoBoy Shrimp Sandwich",
-    descreption: "A large portion of our crispy fried shrimp in steamed sub rolls with tomato, lettuce and topped with our secret Gazebo sauce.",
+    descreption:
+      "A large portion of our crispy fried shrimp in steamed sub rolls with tomato, lettuce and topped with our secret Gazebo sauce.",
     price: "10.99$",
-    img: ""
+    img: "./assets/images/menu/poboy.jpg"
   },
   {
     name: "Beef Hot Dog Small / Large",
-    descreption: "All beef hot dog served with your choice of toppings on a bun ",
+    descreption:
+      "All beef hot dog served with your choice of toppings on a bun ",
     price: "3.99$ / 5.99$",
-    img: ""
+    img: "./assets/images/menu/hotdog.jpg"
   },
   {
     name: "The Ultimate Chicken Steak",
-    descreption: "a big portion of chopped chicken breast seasoned and stuffed with sauteed onions, bell pepper, mushrooms and your favorite chees served in a steamed sub roll.",
+    descreption:
+      "a big portion of chopped chicken breast seasoned and stuffed with sauteed onions, bell pepper, mushrooms and your favorite chees served in a steamed sub roll.",
     price: "10.99$",
-    img: ""
+    img: "./assets/images/menu/cop.jpg"
   },
   {
     name: "The Ultimate Cheese Steak",
-    descreption: "A sliced beef steak loaded with sauteed onion, bell pepper, mushrooms and your favorite cheese served in a steamed sub roll.",
+    descreption:
+      "A sliced beef steak loaded with sauteed onion, bell pepper, mushrooms and your favorite cheese served in a steamed sub roll.",
     price: "10.99$",
-    img: ""
+    img: "./assets/images/menu/cs.jpg"
   },
   {
     name: "The Ultimate Fish Fillet",
-    descreption: " two pieces of crispy golden fried haddock fish served with slaw, tomato, lettuce and topped with our secret Gazebo sauce served on a steamed sub roll.",
+    descreption:
+      " two pieces of crispy golden fried haddock fish served with slaw, tomato, lettuce and topped with our secret Gazebo sauce served on a steamed sub roll.",
     price: "10.99$",
-    img: ""
-  }, {
+    img: "./assets/images/menu/FishSandwich.png"
+  },
+  {
     name: "The Ultimate Souvlaki",
-    descreption: "a big portion of chopped chicken breast, marinated and grilled over our charcoal BBQ, stuffed with red onion, tomato, and a creamy tzatziki sauce served on a steamed sub roll.",
+    descreption:
+      "a big portion of chopped chicken breast, marinated and grilled over our charcoal BBQ, stuffed with red onion, tomato, and a creamy tzatziki sauce served on a steamed sub roll.",
     price: "10.99$",
-    img: ""
+    img: "./assets/images/menu/souvlaki.jpg"
   }
-
 ];
 
-
-var Platters = [{
+var Platters = [
+  {
     name: "Chicken Plate",
-    descreption: "Two whole chicken breasts (Grilled or Fried) cooked to perfection, rested on a mix of sauteed mushrooms, onions and bell pepper served with fries, slaw and texas toast.",
+    descreption:
+      "Two whole chicken breasts (Grilled or Fried) cooked to perfection, rested on a mix of sauteed mushrooms, onions and bell pepper served with fries, slaw and texas toast.",
     price: "12.99$",
-    img: ""
+    img: "./assets/images/menu/ChickenPlate.jpg"
   },
   {
     name: "Shrimp Plate",
-    descreption: "A classic dish of golden fried shrimp served with fries, slaw, texas toast and your choice of tartar sauce or Gazebo sauce.",
+    descreption:
+      "A classic dish of golden fried shrimp served with fries, slaw, texas toast and your choice of tartar sauce or Gazebo sauce.",
     price: "12.99$",
-    img: ""
+    img: "./assets/images/logo-2.png"
   },
   {
     name: "Fish & Chips Plate",
-    descreption: "Two pieces of golden fried haddock fish served with fries, a choice of slaw or tartar sauce & texas toast.",
+    descreption:
+      "Two pieces of golden fried haddock fish served with fries, a choice of slaw or tartar sauce & texas toast.",
     price: "12.99$",
-    img: ""
+    img: "./assets/images/menu/FishNChipsPlate.jpg"
   },
   {
     name: "Hamburger Plate",
-    descreption: "A 9 oz of all beef hamburger patty covered with sauteed mushrooms, onions and bell pepper, topped with gravy and served with fries, slaw and texas toast.",
+    descreption:
+      "A 9 oz of all beef hamburger patty covered with sauteed mushrooms, onions and bell pepper, topped with gravy and served with fries, slaw and texas toast.",
     price: "12.99$",
-    img: ""
+    img: "./assets/images/logo-2.png"
   }
 ];
 
-
-var Gazebobaskets = [{
+var Gazebobaskets = [
+  {
     name: "Buttry Shrimp  8pcs / 16pcs",
-    descreption: "A basket of sauteed, golden fried butterfly shrimp served with choice of sauce.",
+    descreption:
+      "A basket of sauteed, golden fried butterfly shrimp served with choice of sauce.",
     price: "7.99$ / 14.99$",
-    img: ""
+    img: "./assets/images/menu/shrimp-basket.jpg"
   },
   {
     name: "Chicken Wings 6pcs / 12pcs / 18pcs",
     descreption: "with your choice of sauce",
     price: "9.99$ / 16.99$ / 24.99$",
-    img: ""
+    img: "./assets/images/menu/wings.jpg"
   },
   {
     name: "Chicken Tenders 4pcs / 8pcs / 12pcs",
     descreption: "With your choice of sauce.",
     price: "8.99$ / 16.99$ / 24.99$",
-    img: ""
+    img: "./assets/images/menu/tenders.jpg"
   },
   {
     name: "Tempura Cheese Sticks 8pcs / 16pcs",
     descreption: "Crispy mozzarella cheese sticks.",
     price: "7.99$ / 14.99$",
-    img: ""
+    img: "./assets/images/logo-2.png"
   },
   {
     name: "Beef Rolls",
     descreption: "6pcs of Beef rolls",
     price: "7.99$",
-    img: ""
+    img: "./assets/images/logo-2.png"
   },
   {
     name: "Veggie Spring Rolls",
     descreption: "6pcs of Spring rolls",
     price: "7.99$",
-    img: ""
+    img: "./assets/images/logo-2.png"
   }
 ];
 
-
-var Gazebotrios = [{
+var Gazebotrios = [
+  {
     name: "The three musketeers",
     descreption: "A mix of six Wings, four tenders and 8 buttry shrimps",
     price: "23.99$",
-    img: ""
+    img: "./assets/images/menu/3Musketeers.jpg"
   },
   {
     name: "Side Trio",
-    descreption: "Pick three of your favorite french fries, onion rings, sweet potato & potato widges.",
+    descreption:
+      "Pick three of your favorite french fries, onion rings, sweet potato & potato widges.",
     price: "11.99$",
-    img: ""
+    img: "./assets/images/menu/trio.jpg"
   },
   {
     name: "The Rock'n'Roll",
-    descreption: "A mix of beef rolls, Tempura cheese sticks and veggie spring rolls.",
+    descreption:
+      "A mix of beef rolls, Tempura cheese sticks and veggie spring rolls.",
     price: "17.99$",
-    img: ""
+    img: "./assets/images/logo-2.png"
   }
 ];
 
-var Sides = [{
+var Sides = [
+  {
     name: "French Fries",
     descreption: "",
     price: "3.99$",
@@ -263,55 +289,67 @@ var Sides = [{
     descreption: "",
     price: "4.99$",
     img: ""
-  },
+  }
 ];
 
+var Salads = [
+  {
+    name: "Grilled Cheese",
+    descreption: "",
+    price: "3.99$",
+    img: ""
+  },
+  {
+    name: "Grilled Pimento Cheese",
+    descreption: "",
+    price: "4.99$",
+    img: ""
+  },
+  {
+    name: "Coleslaw Pint",
+    descreption: "",
+    price: "4.99$",
+    img: ""
+  },
+  {
+    name: "Garden Salad",
+    descreption:
+      "Lettuce, Tomato, Onion, Pepper, Cheese, Carrot and Cabbage with your choice of italian dressing or Ranch.",
+    price: "6.99$",
+    img: ""
+  },
+  {
+    name: "Caesar Salad",
+    descreption:
+      "A crisp Romaine Lettuce, Croutons, Permeasan cheese with our creamy Caesar dressing.",
+    price: "6.99$",
+    img: ""
+  },
+  {
+    name: "Grilled Chicken Salad",
+    descreption:
+      "Two chopped grilled breasts served on a bed of garden salad with your choice of Rach or Italian dressing.",
+    price: "11.99$",
+    img: ""
+  },
+  {
+    name: "Grilled Caesar Salad",
+    descreption:
+      "Two chopped grilled breasts served on a bed of caesar salad with your choice of Rach or Italian dressing.",
+    price: "11.99$",
+    img: ""
+  },
+  {
+    name: "Fried Shrimp Salad",
+    descreption:
+      "A big portion of crispy golden shrimp served on a bed of garden salad with your choice of Rach or Italian dressing.",
+    price: "11.99$",
+    img: ""
+  }
+];
 
-
-var Salads = [{
-  name: "Grilled Cheese",
-  descreption: "",
-  price: "3.99$",
-  img: ""
-}, {
-  name: "Grilled Pimento Cheese",
-  descreption: "",
-  price: "4.99$",
-  img: ""
-}, {
-  name: "Coleslaw Pint",
-  descreption: "",
-  price: "4.99$",
-  img: ""
-}, {
-  name: "Garden Salad",
-  descreption: "Lettuce, Tomato, Onion, Pepper, Cheese, Carrot and Cabbage with your choice of italian dressing or Ranch.",
-  price: "6.99$",
-  img: ""
-}, {
-  name: "Caesar Salad",
-  descreption: "A crisp Romaine Lettuce, Croutons, Permeasan cheese with our creamy Caesar dressing.",
-  price: "6.99$",
-  img: ""
-}, {
-  name: "Grilled Chicken Salad",
-  descreption: "Two chopped grilled breasts served on a bed of garden salad with your choice of Rach or Italian dressing.",
-  price: "11.99$",
-  img: ""
-}, {
-  name: "Grilled Caesar Salad",
-  descreption: "Two chopped grilled breasts served on a bed of caesar salad with your choice of Rach or Italian dressing.",
-  price: "11.99$",
-  img: ""
-}, {
-  name: "Fried Shrimp Salad",
-  descreption: "A big portion of crispy golden shrimp served on a bed of garden salad with your choice of Rach or Italian dressing.",
-  price: "11.99$",
-  img: ""
-}, ];
-
-
-var Kidsmeal = [{
+var Kidsmeal = [
+  {
     name: "Hot Dog",
     descreption: "Includes kid-sized fries, drink served in a classic car",
     price: "7.99$",
@@ -340,18 +378,21 @@ var Kidsmeal = [{
     descreption: "Includes kid-sized fries, drink served in a classic car",
     price: "7.99$",
     img: ""
-  },
+  }
 ];
 
-var Drinks = [{
+var Drinks = [
+  {
     name: "Soda Drinks",
-    descreption: "pepsi, Diet Pepsi, Mountain Dew, 7UP, Root Beer, Dr.Pepper and Iced Tea",
+    descreption:
+      "pepsi, Diet Pepsi, Mountain Dew, 7UP, Root Beer, Dr.Pepper and Iced Tea",
     price: "2.49$",
     img: ""
   },
   {
     name: "Gazebo Shakes",
-    descreption: "Vanilla, Chocolate, Strawberry, Banana, Cherry, Caramel, Cookie Dough, M&Ms, Chocolate Chips, Mongo, Pineapple, Peanut Butter, Brownie Bites, Cookies'n'Cream and Mixed Nuts",
+    descreption:
+      "Vanilla, Chocolate, Strawberry, Banana, Cherry, Caramel, Cookie Dough, M&Ms, Chocolate Chips, Mongo, Pineapple, Peanut Butter, Brownie Bites, Cookies'n'Cream and Mixed Nuts",
     price: "5.99$",
     img: ""
   },
@@ -385,5 +426,66 @@ var Drinks = [{
     descreption: "",
     price: "3.49$",
     img: ""
+  }
+];
+
+var Desserts = [
+  {
+    name: "Chocolate Lava",
+    descreption: "",
+    price: "7.99$",
+    img: "./assets/images/menu/chocolate-lava.jpg"
   },
-]
+  {
+    name: "Cheesecake",
+    descreption: "",
+    price: "4.99$",
+    img: "./assets/images/menu/cheese-cake.jpg"
+  },
+  {
+    name: "ChocolateBomb",
+    descreption: "",
+    price: "5.99$",
+    img: "./assets/images/menu/chocolate-bomb.jpg"
+  },
+  {
+    name: "Cinger Caramel Spice",
+    descreption: "",
+    price: "5.99$",
+    img: "./assets/images/menu/chocolate-lava.jpg"
+  },
+  {
+    name: "Pistachio Ice Cream",
+    descreption: "",
+    price: "5.99$",
+    img: "./assets/images/menu/pistachio-ice-cream.jpg"
+  },
+  {
+    name: "GC1",
+    descreption:
+      "Three scoops of custard blended with one topping of your choice",
+    price: "5.99$",
+    img: "./assets/images/menu/chocolate-lava.jpg"
+  },
+  {
+    name: "GC2",
+    descreption:
+      "Three scoops of custard blended with two topping of your choice",
+    price: "6.49$",
+    img: "./assets/images/menu/chocolate-lava.jpg"
+  },
+  {
+    name: "Gazebo Sundae",
+    descreption:
+      "Full cup of custard topped with your favorite topping, Whipped cream and cherry",
+    price: "6.49$",
+    img: "./assets/images/menu/chocolate-lava.jpg"
+  },
+  {
+    name: "Banana Split",
+    descreption:
+      "Three scoops on boat dish sided with a aplit banana topped with cherries, strawberries, chocolate chips, whipped creamS",
+    price: "6.99$",
+    img: "./assets/images/menu/chocolate-lava.jpg"
+  }
+];
